d01dd806600beba80a040ae4a59a104eda528321
Plot <49232758+Plot1337@users.noreply.github.com>
2023-07-01 23:41:24 +1000

    Create MiscExt.cs

---
+++ x86Predicate/X86/MiscExt.cs
@@ -0,0 +1,14 @@
+﻿using SharpDisasm.Udis86;
+using SharpDisasm;
+using x86Predicate.X86.Types;
+
+namespace x86Predicate.X86;
+
+internal static class MiscExt
+{
+    public static IX86Operand GetOperand(this Operand arg) => arg.Type switch
+    {
+        ud_type.UD_OP_IMM => new X86ImmediateOperand((int)arg.Value),
+        _ => new X86RegisterOperand((X86Register)arg.Base)
+    };
+}
