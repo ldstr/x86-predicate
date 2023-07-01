ee16a4c50d028d56d2bbae8f982c413bff25c6d1
Plot <49232758+Plot1337@users.noreply.github.com>
2023-07-01 23:14:20 +1000

    Add save method

--- x86Predicate/Program.cs
+++ x86Predicate/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace x86Predicate;
+﻿using dnlib.DotNet.Writer;
+using dnlib.DotNet;
+
+namespace x86Predicate;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+    }
+
+    private static void Save(
+        string file,
+        ModuleDefMD module
+        )
+    {
+        Console.WriteLine("saving module...");
+
+        var writer = new NativeModuleWriterOptions(module, true)
+        {
+            KeepExtraPEData = true,
+            KeepWin32Resources = true,
+            Logger = DummyLogger.NoThrowInstance
+        };
+
+        writer.MetadataOptions.Flags =
+            MetadataFlags.PreserveAll |
+            MetadataFlags.KeepOldMaxStack;
+
+        module.NativeWrite(
+            file + $"_No-x86.exe",
+            writer
+            );
+
+        Console.WriteLine("Saved!");
     }
 }
