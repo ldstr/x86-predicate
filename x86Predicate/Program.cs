55b02dc483bc85b25b9bdeba177512b7bb2888dc
Plot <49232758+Plot1337@users.noreply.github.com>
2023-07-13 01:06:23 +1000

    Use top level statements

--- x86Predicate/Program.cs
+++ x86Predicate/Program.cs
@@ -1,95 +1,90 @@
 ﻿using dnlib.DotNet;
 using dnlib.DotNet.Writer;
+using x86Predicate;
 
-namespace x86Predicate;
-
-internal class Program
+if (args.Length == 0)
 {
-    private static void Main(string[] args)
+    Exit("Usage: x86Predicate <PATH>");
+    return;
+}
+
+var src = args[0];
+
+if (!File.Exists(src))
+{
+    Exit("File not found!");
+    return;
+}
+
+var module = ModuleDefMD.Load(src);
+MethodDefExt.OriginalMD = module;
+
+foreach (var type in module.GetTypes())
+{
+    if (!type.HasMethods)
+        continue;
+
+    foreach (var method in type.Methods)
     {
-        if (args.Length == 0)
-        {
-            Exit("Usage: x86Predicate <PATH>");
-            return;
-        }
+        if (!method.IsNative)
+            continue;
 
-        var src = args[0];
+        Console.WriteLine($"0x{method.MDToken.Raw:X2}");
 
-        if (!File.Exists(src))
-        {
-            Exit("File not found!");
-            return;
-        }
-
-        var module = ModuleDefMD.Load(src);
-        MethodDefExt.OriginalMD = module;
-
-        foreach (var type in module.GetTypes())
-        {
-            if (!type.HasMethods)
-                continue;
-
-            foreach (var method in type.Methods)
-            {
-                if (method.IsNative)
-                    continue;
-
-                Console.WriteLine("0x{0:X2}", method.MDToken.Raw);
-
-                var decrypted = X86ToILConverter.CreateILFromX86Method(
-                    new(method)
-                    );
-
-                method.ImplAttributes = MethodImplAttributes.IL;
-                
-                method.Attributes =
-                    MethodAttributes.FamANDAssem |
-                    MethodAttributes.Family |
-                    MethodAttributes.Static |
-                    MethodAttributes.HideBySig;
-                method.Body = decrypted.Body;
-            }
-        }
-
-        Save(src, module);
-        Exit();
-    }
-
-    private static void Save(
-        string file,
-        ModuleDefMD module
-        )
-    {
-        Console.WriteLine("Saving...");
-
-        var writer = new NativeModuleWriterOptions(module, true)
-        {
-            KeepExtraPEData = true,
-            KeepWin32Resources = true,
-            Logger = DummyLogger.NoThrowInstance
-        };
-
-        writer.MetadataOptions.Flags =
-            MetadataFlags.PreserveAll |
-            MetadataFlags.KeepOldMaxStack;
-
-        module.NativeWrite(
-            file + $"_No-x86.exe",
-            writer
+        var decrypted = X86ToILConverter.CreateILFromX86Method(
+            new(method)
             );
 
-        Console.WriteLine("Saved!");
-    }
+        method.ImplAttributes = MethodImplAttributes.IL;
 
-    private static void Exit(string? msg = null)
+        method.Attributes =
+            MethodAttributes.FamANDAssem |
+            MethodAttributes.Family |
+            MethodAttributes.Static |
+            MethodAttributes.HideBySig;
+
+        method.Body = decrypted.Body;
+    }
+}
+
+Save(src, module);
+Exit();
+
+
+static void Save(
+    string file,
+    ModuleDefMD module
+    )
+{
+    Console.WriteLine("Saving...");
+
+    var writer = new NativeModuleWriterOptions(module, true)
     {
-        Console.WriteLine();
+        KeepExtraPEData = true,
+        KeepWin32Resources = true,
+        Logger = DummyLogger.NoThrowInstance
+    };
 
-        if (!string.IsNullOrEmpty(msg))
-            Console.WriteLine(msg);
+    writer.MetadataOptions.Flags =
+        MetadataFlags.PreserveAll |
+        MetadataFlags.KeepOldMaxStack;
 
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey(true);
-        Environment.Exit(0);
-    }
+    module.NativeWrite(
+        file + $"_No-x86.exe",
+        writer
+        );
+
+    Console.WriteLine("Saved!");
+}
+
+static void Exit(string? msg = null)
+{
+    Console.WriteLine();
+
+    if (!string.IsNullOrEmpty(msg))
+        Console.WriteLine(msg);
+
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey(true);
+    Environment.Exit(0);
 }
